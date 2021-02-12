6189b58c5f95b874b1d622106cfaecd4c9504f29
TomSjouw <tom.sjouw@student.hu.nl>
2021-02-12 19:49:14 +0100

    Practicum 1 opdracht 5

---
+++ src/Practicum1/opdracht5/Opdracht5.java
@@ -0,0 +1,10 @@
+package Practicum1.opdracht5;
+
+public class Opdracht5 {
+    public static void main(String[] args)  {
+        for(int a=1; a<10; a+=1)
+            for(a=1; a<6; a+=1)
+                if (a-(a/2+a/2)==0)
+                    System.out.println("s\nss");
+    }
+}
